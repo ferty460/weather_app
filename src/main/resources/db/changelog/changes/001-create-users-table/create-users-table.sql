3f8b1595798f0af7e87e1d179eecf30a0a7270f4
ferty460 <rostovcevvladimir460@gmail.com>
2025-11-09 16:10:06 +0300

    feat: added migration for users with creating and filling table

---
+++ src/main/resources/db/changelog/changes/001-create-users-table/create-users-table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS users
+(
+    id       BIGSERIAL PRIMARY KEY,
+    login    VARCHAR(128) NOT NULL,
+    password VARCHAR(64)  NOT NULL
+);
+
+COMMENT ON TABLE users IS 'Таблица для хранения информации о пользователях';
+COMMENT ON COLUMN users.id IS 'Уникальный идентификатор пользователя';
+COMMENT ON COLUMN users.login IS 'Логин пользователя, username или email';
+COMMENT ON COLUMN users.password IS 'Пароль пользователя';
