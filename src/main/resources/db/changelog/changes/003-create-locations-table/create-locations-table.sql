86aff1918cf3a7b96e3d1db2dd32485f2c77dca9
ferty460 <rostovcevvladimir460@gmail.com>
2025-11-09 16:10:28 +0300

    feat: added migration for locations with creating and filling table

---
+++ src/main/resources/db/changelog/changes/003-create-locations-table/create-locations-table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS locations
+(
+    id        BIGSERIAL PRIMARY KEY,
+    name      VARCHAR(128) NOT NULL,
+    user_id   BIGINT       NOT NULL,
+    latitude  NUMERIC(10, 8),
+    longitude NUMERIC(11, 8)
+);
+
+COMMENT ON TABLE locations IS 'Таблица для хранения географических локаций пользователей';
+COMMENT ON COLUMN locations.id IS 'Уникальный идентификатор локации';
+COMMENT ON COLUMN locations.name IS 'Название локации';
+COMMENT ON COLUMN locations.user_id IS 'Внешний ключ на пользователя';
+COMMENT ON COLUMN locations.latitude IS 'Географическая широта';
+COMMENT ON COLUMN locations.longitude IS 'Географическая долгота';
