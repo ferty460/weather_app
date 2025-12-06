1d078b348bf10e87088a0b886cff5be23b9dac77
ferty460 <rostovcevvladimir460@gmail.com>
2025-12-06 18:46:26 +0300

    feat: add `/register`, `/login` and `/logout` endpoints in `AuthController`

--- src/main/java/org/example/weatherapp/controller/AuthController.java
+++ src/main/java/org/example/weatherapp/controller/AuthController.java
@@ -1,5 +1,7 @@
 package org.example.weatherapp.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.example.weatherapp.dto.UserDto;
 import org.example.weatherapp.service.AuthService;
@@ -15,6 +17,16 @@ public class AuthController {
     @PostMapping("/register")
     public UserDto register(@RequestBody UserDto userDto) {
         return authService.register(userDto);
+    }
+
+    @PostMapping("/login")
+    public void login(@RequestBody UserDto userDto, HttpServletResponse response) {
+        authService.login(userDto, response);
+    }
+
+    @PostMapping("/logout")
+    public void logout(HttpServletRequest request, HttpServletResponse response) {
+        authService.logout(request, response);
     }
 
 }
