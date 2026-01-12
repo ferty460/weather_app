111c933f1973100b5db3331334e01ac790d07934
ferty460 <rostovcevvladimir460@gmail.com>
2026-01-12 18:55:22 +0300

    feat: locations search

--- src/main/java/org/example/weatherapp/controller/OpenWeatherApiController.java
+++ src/main/java/org/example/weatherapp/controller/OpenWeatherApiController.java
@@ -1,26 +1,34 @@
 package org.example.weatherapp.controller;
 
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.example.weatherapp.dto.response.LocationResponse;
 import org.example.weatherapp.dto.response.WeatherResponse;
 import org.example.weatherapp.service.OpenWeatherApiService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 import java.util.List;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class OpenWeatherApiController {
 
     private final OpenWeatherApiService apiService;
 
     @GetMapping("/locations/search")
-    public List<LocationResponse> searchLocations(@RequestParam("query") @NotBlank String location) {
-        return apiService.searchLocationsByName(location);
+    public String searchLocations(@RequestParam("query") @NotBlank String location, Model model) {
+        List<LocationResponse> locations = apiService.searchLocationsByName(location);
+        model.addAttribute("locations", locations);
+        model.addAttribute("query", location);
+
+        return "search_result";
     }
 
     @GetMapping("/weather")
