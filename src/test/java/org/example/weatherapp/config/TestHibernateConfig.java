561f4ab6563172803b2cc84fc039d99b63ef512b
ferty460 <rostovcevvladimir460@gmail.com>
2026-01-27 21:07:27 +0300

    test: add test configs

---
+++ src/test/java/org/example/weatherapp/config/TestHibernateConfig.java
@@ -0,0 +1,75 @@
+package org.example.weatherapp.config;
+
+import lombok.RequiredArgsConstructor;
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+@ComponentScan(basePackages = {
+        "org.example.weatherapp.repository",
+        "org.example.weatherapp.service",
+        "org.example.weatherapp.mapper"
+})
+@PropertySource("classpath:test_application.properties")
+@RequiredArgsConstructor
+public class TestHibernateConfig {
+
+    private final Environment env;
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .setName("testdb;MODE=PostgreSQL")
+                .build();
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory(DataSource dataSource) {
+        LocalSessionFactoryBean factory = new LocalSessionFactoryBean();
+        factory.setDataSource(dataSource);
+        factory.setPackagesToScan("org.example.weatherapp");
+        factory.setHibernateProperties(hibernateProperties());
+        return factory;
+    }
+
+    @Bean
+    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+        return new HibernateTransactionManager(sessionFactory);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    private Properties hibernateProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", env.getProperty("hibernate.dialect"));
+        properties.put("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
+        properties.put("hibernate.format_sql", env.getProperty("hibernate.format_sql"));
+        properties.put("hibernate.highlight_sql", env.getProperty("hibernate.highlight_sql"));
+        properties.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
+        properties.put("hibernate.validator.apply_to_ddl", env.getProperty("hibernate.validator.apply_to_ddl"));
+        properties.put("jakarta.persistence.validation.mode", env.getProperty("jakarta.persistence.validation.mode"));
+
+        return properties;
+    }
+
+}
+
